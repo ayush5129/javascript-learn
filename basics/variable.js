3d782abe8ad31b570202dd17dd85acc5bf703e0d
Ayush Kumar Singh <130050135+ayush5129@users.noreply.github.com>
2025-03-19 03:46:14 +0000

    vari

---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+const val = 12548
+let city = "varanasi"
+var state = "uttar pradesh"
+pin = 125.36
+
+
+// val = 58923; // not allowed to change const
+city = "Allahabad" // you can change it
+console.log([val,city,state,pin]);
