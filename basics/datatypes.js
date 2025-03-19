4c550c60c4d72de505a0fdc42b008608d677de6a
Ayush Kumar Singh <130050135+ayush5129@users.noreply.github.com>
2025-03-19 04:22:07 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+let age = 45.23
+let isAdult = true
+let name = "Ayush"
+let gf = null
+let city;
+
+console.log(typeof city)
+console.log(typeof gf) 
+console.log(typeof "manisha")
+console.log(typeof age)
+
+// null has object type 
+// undefined has umdefined type 
+
+
