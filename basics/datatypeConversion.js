0e0372c54f983acb2310c91bea509672d0ff3278
Ayush Kumar Singh <130050135+ayush5129@users.noreply.github.com>
2025-03-20 03:32:30 +0000

    modified

--- basics/datatypeConversion.js
+++ basics/datatypeConversion.js
@@ -1,48 +1,67 @@
-// Number to string
-let score = 33;
-console.log(typeof score);
-let valueInString = String(score);
-console.log( typeof valueInString);
+// // Number to string
+// let score = 33;
+// console.log(typeof score);
+// let valueInString = String(score);
+// console.log( typeof valueInString);
 
-// String to Number
-let card = "33"
-console.log(typeof card);
-let valueInNumber = Number(card);
-console.log(typeof valueInNumber);
+// // String to Number
+// let card = "33"
+// console.log(typeof card);
+// let valueInNumber = Number(card);
+// console.log(typeof valueInNumber);
 
-// if we have a string in which i have number and alphabet both are present then -> output is NaN
-let mix = "546asd";
-console.log(typeof mix);
-let changeInNumber = Number(mix);
-console.log(changeInNumber);
+// // if we have a string in which i have number and alphabet both are present then -> output is NaN
+// let mix = "546asd";
+// console.log(typeof mix);
+// let changeInNumber = Number(mix);
+// console.log(changeInNumber);
 
-// boolean to number
-let isLoggedIn = true
-console.log(typeof isLoggedIn);
-let inNumber = Number(isLoggedIn)
-console.log(inNumber);
-
-
-// number to boolean, string to boolean
-let val = 15
-console.log(typeof val);
-let changeInBoolean = Boolean(val);
-console.log(changeInBoolean);
-
-// in case of empty string give false
-let num = ""
-let changeInBoolean1 = Boolean(num);
-console.log(changeInBoolean1);
-
-/// otherwise any string give true
-let naam = "Ayush"
-let stringtoBoolean = Boolean(naam);
-console.log(stringtoBoolean);
-
-// in case of null and undefined output is false
-let num1 = undefined
-let changeInBoolean2 = Boolean(num);
-console.log(changeInBoolean2);
+// // boolean to number
+// let isLoggedIn = true
+// console.log(typeof isLoggedIn);
+// let inNumber = Number(isLoggedIn)
+// console.log(inNumber);
 
 
+// // number to boolean, string to boolean
+// let val = 15
+// console.log(typeof val);
+// let changeInBoolean = Boolean(val);
+// console.log(changeInBoolean);
 
+// // in case of empty string give false
+// let num = ""
+// let changeInBoolean1 = Boolean(num);
+// console.log(changeInBoolean1);
+
+// /// otherwise any string give true
+// let naam = "Ayush"
+// let stringtoBoolean = Boolean(naam);
+// console.log(stringtoBoolean);
+
+// // in case of null and undefined output is false
+// let num1 = undefined
+// let changeInBoolean2 = Boolean(num);
+// console.log(changeInBoolean2);
+
+
+/// string operation
+// console.log(8+2);
+// console.log(8-2);
+// console.log(8*2);
+// console.log(8/2);
+// console.log(8**2);
+// console.log(8%2);
+// console.log(7%9);
+
+
+// console.log(+true);
+// console.log(+"");
+
+// some other operation
+console.log(1 + 2)
+console.log(1 + "2")
+console.log("1" + 2)
+console.log(1 + "2"+"5" + 6)
+console.log(1 + 6 + "2"+"5" )
+console.log("1" + 2 + 3 + "8");
