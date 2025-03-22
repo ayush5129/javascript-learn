8efcd9664d72d63ac14b47fbd458f49cfd7ae1f0
Ayush Kumar Singh <130050135+ayush5129@users.noreply.github.com>
2025-03-22 12:39:09 +0000

    func2

---
+++ basics2/function2.js
@@ -0,0 +1,48 @@
+// rest operator ----> if you want to print without rest operator then you got only 100 as output
+// but using rest(...) operator you can print all of them
+// function printNum(...num1){
+//     return num1;
+// }
+// console.log(printNum(100,200,830,50));
+
+
+// function printNum(val1,val2,...num1){
+//     return num1;
+// }
+// console.log(printNum(100,200,830,50)); // give [830,50] beacuse this is rest value 
+
+//object in function 
+
+// const user = {
+//     username:"ayush",
+//     price:456
+// }
+// function handleObject(anyobject){
+//     console.log(`LoggedIn user ${anyobject.username} and price is their subscription is ${anyobject.price}`)
+// }
+// handleObject(user);
+
+
+// you have no need to made first object.. you can write the object when you call the function
+// function handleObject(anyobject){
+//     console.log(`LoggedIn user ${anyobject.username} and price is their subscription is ${anyobject.price}`)
+// }
+// handleObject({
+//     username:"Mannu",
+//     price:789
+// })
+
+// Array in function 
+// const arr = [100,20,450,600]
+// function handleArray(getArray){
+//     return getArray[2]
+// }
+// console.log(handleArray(arr))
+
+// you have no need to made first array.. you can write the array when you call the function
+// function handleArray(getArray){
+//     return getArray[2]
+// }
+// console.log(handleArray([100,20,450,600]))
+
+
