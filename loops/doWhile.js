f3a3e609b13dcb5cb67b416ffa0c8872301ee3f0
Ayush Kumar Singh <130050135+ayush5129@users.noreply.github.com>
2025-03-24 06:38:30 +0000

    doWhile

---
+++ loops/doWhile.js
@@ -0,0 +1,35 @@
+// while loop
+//syntax
+// while (condition) {
+    
+// }
+
+// let score = 0;
+// while(score <= 10){
+//     console.log(`score is ${score}`)
+//     score += 2
+// }
+
+// while loop with array
+// let arr = ["Ayush","Manisha","Sonu","Mannu"]
+// let val = 0
+// while (val < arr.length) {
+//     console.log(`Name is ${arr[val]}`)
+//     val++
+// }
+
+//**************************** */
+// do While loop
+// syntax 
+// do{
+   
+// }while (condition) {
+    
+// }
+
+
+// let score = 0;
+// do{
+//     console.log(`my score is ${score}`)
+//     score+=1
+// }while(score <= 10)
