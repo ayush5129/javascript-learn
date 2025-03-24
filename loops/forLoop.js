e55394258259a3c446dcf763da75cbbb7e989c9f
Ayush Kumar Singh <130050135+ayush5129@users.noreply.github.com>
2025-03-24 06:23:24 +0000

    forLoop

---
+++ loops/forLoop.js
@@ -0,0 +1,34 @@
+// for
+// let array = [10,20,54,23,25];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element)
+// }
+
+// table printing from 0 to 10
+// for(let i = 0;i<=10;i++){
+//     console.log(`outer loop ${i}`)
+//     for(let j = 0;j<=10;j++){
+//     //    console.log(`inner  loop value ${j} and inner value${i}`)
+//     console.log(i + '*' + j + ' = ' + i*j)
+//     }
+// }
+
+// break and continue
+// break
+// for (let index = 1; index <= 20; index++) {
+//    if(index == 5){
+//     console.log(`detected 5`)
+//     break
+//    }
+//    console.log(`value of i is ${index}`); 
+// }
+
+// continue
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//      console.log(`detected 5`)
+//      continue
+//     }
+//     console.log(`value of i is ${index}`); 
+//  }
