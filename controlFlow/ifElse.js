9982612a1b966b400b5a06f4206d1c91cd8c067c
Ayush Kumar Singh <130050135+ayush5129@users.noreply.github.com>
2025-03-24 04:35:31 +0000

    condition

---
+++ controlFlow/ifElse.js
@@ -0,0 +1,64 @@
+// if --> eske under ka code tabhi run hoga jb condition true ho
+// if(true){
+//     console.log("always execued")
+// }
+
+// let userLoggedIn = true;
+// if(userLoggedIn){
+//     console.log("Ye bhi print hoga kyuki value true hai userLoggedIn ki")
+// }
+
+// most of the time hm koi conditional operator use krte hai
+// <,> <=,>=,!=,==,===
+// == check only values 
+// === check value + datatype also
+// if(2 == "2"){
+//     console.log("exceuted");
+// }
+// if(2 === "2"){
+//     console.log("Not exceuted bcaz datatype are not matched");
+// }
+// if(3 != 2){
+//     console.log("see the use of conditional opertor in if else");
+// }
+
+// let temp = 50
+// if(temp < 50){
+//     console.log("Temp is less than fifty")
+// }
+// else{
+//     console.log("temperature is greater tha fifty")
+// }
+
+// shortend --- > without curlly braces
+// const balance = 400
+// if(balance > 200) console.log("Yes greater than 200")
+
+// if you have more than 1 condition -- > then use else if..... you can use if or else if without else statement
+// const balance = 1000
+// if(balance < 500){
+//      console.log("balance is less than 500")
+// }
+// else if(balance < 700){
+//     console.log("balance is less than 700")
+// }
+// else if(balance < 1200){
+//     console.log("balance is less than 1200")
+// }
+// else{
+//     console.log("invalid balance")
+// }
+
+// && and || operator  
+// const loggedIn = true 
+// const debitCard =  true
+// const googleLogin = false
+// const emailLogin = true
+
+// if(loggedIn && debitCard ){ // and operator me dono condition true honi chahiye
+//     console.log("Allow to buy course")
+// }
+// if(googleLogin || debitCard){ //  or operator me koi ek bhi true hoga to bhi execute ho jayega code
+//     console.log("user loggin")
+// }
+
