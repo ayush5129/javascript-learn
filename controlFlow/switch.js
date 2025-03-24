40ebae9a641053461521e674b4ea4f0ab6af850a
Ayush Kumar Singh <130050135+ayush5129@users.noreply.github.com>
2025-03-24 04:43:03 +0000

    switch

---
+++ controlFlow/switch.js
@@ -0,0 +1,23 @@
+// switch  
+
+// const month = 3;
+// switch(3){
+//     case 1:
+//         console.log("Janurary")
+//         break;
+//     case 2:
+//         console.log("Febraury")
+//         break;
+//     case 3:
+//         console.log("March")
+//         break;
+//     case 4:
+//         console.log("April")
+//         break;
+//     default:
+//         console.log("Invalid month")
+// }
+
+// same thing u can use with string but remember case sensivity
+
+
